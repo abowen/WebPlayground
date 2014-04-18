c54eb543569a4ac7c712546950edfffe3b379892
abowen <andrew.james.bowen@gmail.com>
2014-04-18 20:49:23 +1000

    Added Phaser + Hello World

---
+++ WebPlayground/Games/helloworld.js
@@ -0,0 +1,21 @@
+﻿var game = new Phaser.Game(500, 600, Phaser.AUTO, 'game_div');
+
+var main_state = {
+
+    preload: function () {
+        game.stage.backgroundColor = '#71c5cf';
+        game.load.image('hello', 'assets/hello.png');        
+    },
+
+    create: function () {
+        this.bird = this.game.add.sprite(250, 300, 'hello');        
+    },
+
+    update: function () {
+        this.bird.angle += 1;
+    }    
+}
+
+
+game.state.add('main', main_state);
+game.state.start('main');
