2d3068c01d031fce15d3d27b7d79c25a5ba8953b
abowen <andrew.james.bowen@gmail.com>
2014-05-13 22:26:21 +1000

    Simplified JavaScript & added food examples

--- WebPlayground/JavaScript/Knockout/knockout.js
+++ WebPlayground/JavaScript/Knockout/knockout.js
@@ -1,32 +1,27 @@
 ﻿
 $(function () {
 
-    // Common Food Calories
-    var foodData = {};
-    foodData.getData = (function () {
-        var result = ko.observableArray([
-        { name: "Milk", calories: "69", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+milk" },
-        { name: "Skim Milk", calories: "39", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+skim+milk" },
-        { name: "Beer", calories: "34", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+beer" },
-            
-        ]);
-        return result;
-    })();
+    // Common Food
+    var foodData = ko.observableArray([
+    { name: "Chicken Thigh", calories: "235", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+chicken+thigh" },
+    { name: "Lamb", calories: "78", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+lamb" },
+    { name: "Duck", calories: "269", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+duck" },
 
-    // Observable
-    var myViewModel = {        
-        food:  ko.observableArray([])
-    };   
+    ]);
 
-    myViewModel.loadData = function() {
-        var data = foodData.getData();
-        $.each(data, function(i, c) {
-            myViewModel.food.push(c);
-        });
+    // Common Drinks
+    var drinkData = ko.observableArray([
+    { name: "Milk", calories: "69", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+milk" },
+    { name: "Skim Milk", calories: "39", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+skim+milk" },
+    { name: "Beer", calories: "34", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+beer" },
+
+    ]);
+
+    // Binding
+    var myViewModel = {
+        food: foodData,
+        drink: drinkData
     };
 
-    myViewModel.loadData();
-
     ko.applyBindings(myViewModel);
-
 });
