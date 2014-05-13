484ea44e70e8fcb4dede3f5ba349ba24b311536f
abowen <andrew.james.bowen@gmail.com>
2014-05-13 22:35:07 +1000

    Added URL reference to Wolfram

--- WebPlayground/JavaScript/Knockout/knockout.js
+++ WebPlayground/JavaScript/Knockout/knockout.js
@@ -3,17 +3,17 @@ $(function () {
 
     // Common Food
     var foodData = ko.observableArray([
-    { name: "Chicken Thigh", calories: "235", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+chicken+thigh" },
-    { name: "Lamb", calories: "78", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+lamb" },
-    { name: "Duck", calories: "269", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+duck" },
+    { name: "Chicken Thigh", calories: "235", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+chicken+thigh" },
+    { name: "Lamb", calories: "78", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+lamb" },
+    { name: "Duck", calories: "269", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+duck" },
 
     ]);
 
     // Common Drinks
     var drinkData = ko.observableArray([
-    { name: "Milk", calories: "69", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+milk" },
-    { name: "Skim Milk", calories: "39", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+skim+milk" },
-    { name: "Beer", calories: "34", reference: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+beer" },
+    { name: "Milk", calories: "69", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+milk" },
+    { name: "Skim Milk", calories: "39", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+skim+milk" },
+    { name: "Beer", calories: "34", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+beer" },
 
     ]);
 
