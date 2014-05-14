92ab20fcb69a7c36badbcc5c7f5c92a2ece6c3e7
abowen <andrew.james.bowen@gmail.com>
2014-05-14 19:54:59 +1000

    Added more measurements

--- WebPlayground/JavaScript/Knockout/knockout.js
+++ WebPlayground/JavaScript/Knockout/knockout.js
@@ -4,8 +4,11 @@ $(function () {
     // Common Food
     var foodData = ko.observableArray([
     { name: "Chicken Thigh", calories: "235", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+chicken+thigh" },
-    { name: "Lamb", calories: "78", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+lamb" },
+    { name: "Chicken Breast", calories: "175", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+chicken+breast" },
+    { name: "Lamb", calories: "251", url: "https://www.wolframalpha.com/input/?i=calories+in+100g+of+lamb&a=*DPClash.ExpandedFoodE.lamb-_*PreparedLamb-" },
     { name: "Duck", calories: "269", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+duck" },
+    { name: "Sirloin Steak", calories: "211", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100g+of+sirloin+steak" },
+        
 
     ]);
 
@@ -14,7 +17,7 @@ $(function () {
     { name: "Milk", calories: "69", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+milk" },
     { name: "Skim Milk", calories: "39", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+skim+milk" },
     { name: "Beer", calories: "34", url: "https://www.wolframalpha.com/input/?i=how+many+calories+in+100ml+of+beer" },
-
+    { name: "Rum", calories: "219", url: "https://www.wolframalpha.com/input/?i=calories+in+100ml+of+rum" },    
     ]);
 
     // Binding
